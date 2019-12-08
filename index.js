3d4777c6f1cf790537843f0b73804be33002bd62
ZZQ <ZZGaming@list.ru>
2019-12-08 15:04:52 +0800

    Add parse method

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 /*
 Config for ss:// style
 {
-  method: string method name
-  password: password for shadoworks
-  hostname: hostname damain
-  port: undefined
-  name: undefined
+    "server":"my_server_ip",
+    "server_port":8388,
+    "password":"mypassword",
+    "method":"aes-256-cfb",
+    "name":"my server"
 }
 Config for ssr:// style
 {
-  method: string method name
-  password: password for shadoworks
-  hostname: hostname damain
-  port: undefined
-  name: undefined
+    "server":"my_server_ip",
+    "server_port":8388,
+    "password":"mypassword",
+    "method":"aes-256-cfb",
+    "name":"my server",
+    "protocol": "protocolName",
+    "obfs": "",
+    "obfs_param": "",
+    "protocol_param": ""
+    "group": ""
 }
 */
 
 class Base64 {
   encode(string) {
+    if (!string) string = ''
     return Buffer.from(string).toString('base64')
   }
 
@@ -29,12 +35,11 @@ class Base64 {
 
 const base64 = new Base64()
 
-class SsLink extends Base64 {
+class SsLink {
   static genSS(config) {
-    // Y2FtZWxsaWEtMTI4LWNmYjpQQVNTV09SRA@HOSTNAME:8388#NAME
     const c = config
     // METHOD:PASSWORD@HOSTNAME:PORT#NAME
-    const URI = `${base64.encode(`${c.method}:${c.password}`)}@${c.hostname}:${c.port}#${encodeURI(c.name)}`
+    const URI = `${base64.encode(`${c.method}:${c.password}`)}@${c.server}:${c.server_port}#${encodeURI(c.name)}`
     const URL = 'ss://' + URI
     return URL
   }
@@ -42,14 +47,44 @@ class SsLink extends Base64 {
   static genSSR(config) {
     const c = config
     // ssr://HOSTNAME:PORT:PROTOCOL:METHOD:OBFS:PASSWORD/?obfsparam=OBFS_PARAM_BASE64&protoparam=PROTO_PARAM_BASE64&remarks=NAME_BASE64&group=GROUP_BASE64
-    const URI = `${c.hostname}:${c.port}:${c.protocol}:${c.method}:${base64.encode(c.obfs)}:${c.password}/?obfsparam=${base64.encode(c.obfs_param)}&protoparam=${base64.encode(c.protocol_param)}&remarks=${base64.encode(c.name)}&group=${base64.encode(c.group)}`
+    const URI = `${c.server}:${c.server_port}:${c.protocol}:${c.method}:${base64.encode(c.obfs)}:${c.password}/?obfsparam=${base64.encode(c.obfs_param)}&protoparam=${base64.encode(c.protocol_param)}&remarks=${base64.encode(c.name)}&group=${base64.encode(c.group)}`
     const URL = 'ssr://' + base64.encode(URI)
     return URL
   }
 
-  // TODO: Gen SSS Links
-  // TODO: Parse SS Links
-  // TODO: Parse SSR Links
+  static parse(link) {
+    let parseInfo = {}
+
+    if (link.startsWith('ss://')) {
+      const URI = link.replace('ss://', '').split('@')
+
+      parseInfo.method/*------*/= base64.decode(URI[0]).split(':')[0]
+      parseInfo.password/*----*/= base64.decode(URI[0]).split(':')[1]
+      parseInfo.server/*------*/= URI[1].split('#')[0].split(':')[0]
+      parseInfo.server_port/*-*/= URI[1].split('#')[0].split(':')[1]
+      parseInfo.name/*--------*/= decodeURI(URI[1].split('#')[1])
+
+    } else if (link.startsWith('ssr://')) {
+      const URI = base64.decode(link.replace('ssr://', '')).split(':')
+      const params = new URLSearchParams(URI[5].split('/')[1])
+
+      parseInfo.server/*---------*/= URI[0]
+      parseInfo.server_port/*----*/= URI[1]
+      parseInfo.protocol/*-------*/= URI[2]
+      parseInfo.method/*---------*/= URI[3]
+      parseInfo.obfs/*-----------*/= base64.decode(URI[4])
+      parseInfo.password/*-------*/= URI[5].split('/')[0]
+      parseInfo.name/*-----------*/= base64.decode(params.get('remarks'))
+      parseInfo.obfs_param/*-----*/= base64.decode(params.get('obfsparam'))
+      parseInfo.protocol_param/*-*/= base64.decode(params.get('protoparam'))
+      parseInfo.group/*----------*/= base64.decode(params.get('group'))
+    }
+
+    return parseInfo
+  }
+
+  // TODO: Gen SSS Link
+  // TODO: validate Link
 }
 
 module.exports = SsLink
